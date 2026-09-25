eb41ef0bbdb9f8a76eecdbaf9812e6873ccebed0
Andrew <andrew.telnov@gmail.com>
2026-09-25 11:47:36 +0300

    Delete file

--- playwright.tmp-3002.config.ts
+++
@@ -1,7 +0,0 @@
-import base from "./playwright.config";
-export default {
-  ...base,
-  reporter: [["list"]],
-  use: { ...base.use, baseURL: "http://localhost:3002" },
-  webServer: { ...base.webServer, command: "npx next start -p 3002", url: "http://localhost:3002", reuseExistingServer: false },
-};
